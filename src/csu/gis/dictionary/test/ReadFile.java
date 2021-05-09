38b9c3dbdcc5f9cba25aa9c3db06197823286c27
code lab <65063861+CodeLabworld@users.noreply.github.com>
2021-05-10 07:47:42 +0800

    Add files via upload

---
+++ src/csu/gis/dictionary/test/ReadFile.java
@@ -0,0 +1,15 @@
+package csu.gis.dictionary.test;
+
+import csu.gis.dictionary.dic.WordList;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadFile {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String rootFilePath = "";
+        System.out.println(new WordList(rootFilePath + "langdao-ce-gb.txt", 1));
+    }
+
+}
